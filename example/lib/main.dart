f0c8ea9df0e89a5ed85b5f1e078259d3c48927be
sumitsharansatsangi <sharansumitkumar@gmail.com>
2024-10-03 22:57:07 +0530

    code updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,15 +1,11 @@
-// import 'dart:io';
-import 'dart:io';
 import 'dart:typed_data';
-
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:dbx/dbx.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await DBX.init();
- 
-
+  await DBX.init();
   runApp(MyApp());
 }
 
@@ -37,33 +33,41 @@ class MyAppState extends State<MyApp> {
           centerTitle: true,
         ),
         body: Center(
-            child: Column(
-          children: [
-            if (profilePic != null && profilePic.isNotEmpty)
-              Image.memory(Uint8List.fromList(profilePic)),
-            Text("Message: ${DBX.getString('message') ?? 'No data'}"),
-            Text(
-                "Username: ${DBX.getString('username') ?? 'No Data'}"), // Output: john_doe
-            Text("Age: ${DBX.getInt('age')}"), // Output: 30
-            Text("Height: ${DBX.getDouble('height')}"), // Output: 5.9
-            Text("IsAdmin: ${DBX.getBool('isAdmin')}"), // Output: true
-            Text(
-                "Tags: ${DBX.getStringList('tags') ?? [].join(',')}"), // Output: [dart, flutter]
-            Text(
-                "Scores: ${DBX.getIntList('scores') ?? [].join(',')}"), // Output: [100, 95, 85]
-            Text(
-                "Strike Rate: ${DBX.getDoubleList('strike_rate')}"), // Output: [36.6, 37.0, 36.8]
-            Text(
-                "Attendence: ${DBX.getBoolList('attendence')}"), // Output: [true, false, true]
-            if (files != null)
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  for (final file in files)
-                    Image.memory(Uint8List.fromList(file)),
-                ],
-              ),
-          ],
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              if (profilePic != null && profilePic.isNotEmpty)
+                Image.memory(Uint8List.fromList(profilePic)),
+              Text("Message: ${DBX.getString('message') ?? 'No data'}"),
+              Text(
+                  "Username: ${DBX.getString('username') ?? 'No Data'}"), // Output: john_doe
+              Text("Age: ${DBX.getInt('age')}"), // Output: 30
+              Text("Height: ${DBX.getDouble('height')}"), // Output: 5.9
+              Text("IsAdmin: ${DBX.getBool('isAdmin')}"), // Output: true
+              Text(
+                  "Tags: ${DBX.getStringList('tags') ?? [].join(',')}"), // Output: [dart, flutter]
+              Text(
+                  "Scores: ${DBX.getIntList('scores') ?? [].join(',')}"), // Output: [100, 95, 85]
+              Text(
+                  "Strike Rate: ${DBX.getDoubleList('strike_rate')}"), // Output: [36.6, 37.0, 36.8]
+              Text(
+                  "Attendence: ${DBX.getBoolList('attendence')}"), // Output: [true, false, true]
+              if (files != null)
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      for (final file in files)
+                        Image.memory(Uint8List.fromList(file),
+                            fit: BoxFit.contain,
+                            width: (MediaQuery.of(context).size).width /
+                                files.length),
+                    ],
+                  ),
+                ),
+            ],
+          ),
         )),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: writeData,
@@ -80,16 +84,17 @@ class MyAppState extends State<MyApp> {
     DBX.setInt('age', 30);
     DBX.setDouble('height', 5.9);
     DBX.setBool('isAdmin', true);
-    DBX.setBytes(
-        'profilePicture', File('assets/profile.jpeg').readAsBytesSync());
+    DBX.setBytes('profilePicture',
+        (await rootBundle.load('assets/profile.jpeg')).buffer.asUint8List());
     DBX.setStringList('tags', ['dart', 'flutter']);
     DBX.setIntList('scores', [100, 95, 85]);
     DBX.setDoubleList('strike_rate', [36.6, 37.0, 36.8]);
     DBX.setBoolList('attendence', [true, false, true]);
     DBX.setBytesList('files', [
-      File('assets/file1.jpeg').readAsBytesSync(),
-      File('assets/logo.jpeg').readAsBytesSync(),
-      File('assets/file2.jpeg').readAsBytesSync()
+      (await rootBundle.load('assets/profile.jpeg')).buffer.asUint8List(),
+      (await rootBundle.load('assets/logo.jpeg')).buffer.asUint8List(),
+      (await rootBundle.load('assets/file1.jpeg')).buffer.asUint8List(),
+      (await rootBundle.load('assets/file2.jpeg')).buffer.asUint8List(),
     ]);
     setState(() {});
   }
